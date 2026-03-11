f4e1d24ceab1ff69c8b1a95e4b31af53c1667b14
pujan-X <pujansuthar345@gmail.com>
2026-03-11 11:01:07 +0530

    Remove comment from StudentRepository interface

--- src/main/java/com/edupredict/repository/StudentRepository.java
+++ src/main/java/com/edupredict/repository/StudentRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    // Custom query to find students by risk level
     List<Student> findByRiskLevel(Student.RiskLevel level);
 }
